b4f64004c991081f967db68527768120808b2b4d
archiblue22 <paguirre@byui.edu>
2021-09-16 10:55:10 -0600

    commit

--- prove01-routes.js
+++ prove01-routes.js
@@ -1,48 +1,40 @@
 const fs = require('fs');
 
-const requestHandler = (req, res) => {
+const server = http.createServer((req, res) => {
     const url = req.url;
-    const method = req.method;
     if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Hello from my Node.js Server!</h1>');
+        res.write(
+            '<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+        );
         res.write('</html>');
         return res.end();
     }
-    if (url === '/message' && method === 'POST') {
+    if (url === '/users') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>User List</title></head>');
+        res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/create-user') {
         const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
+        req.on('data', chunk => {
             body.push(chunk);
         });
-        return req.on('end', () => {
+        req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
+            console.log(parsedBody.split('=')[1]);
         });
+        res.statusCode = 302;
+        res.setHeader('Location', '/users');
+        res.end();
     }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-};
+});
 
-// module.exports = requestHandler;
-
-// module.exports = {
-//     handler: requestHandler,
-//     someText: 'Some hard coded text'
-// };
-
-// module.exports.handler = requestHandler;
-// module.exports.someText = 'Some hard coded text';
-
-exports.handler = requestHandler;
-exports.someText = 'Some hard coded text';
+module.exports.handler = requestHandler;
+module.exports.someText = server;
